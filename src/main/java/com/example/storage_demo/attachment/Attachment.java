101201f06b3e8d2f74124d8dc3303a62ad051136
ABaulderstone <alex.baulderstone@gmail.com>
2025-03-31 11:52:51 +1100

    adds base entity and attachment entity

---
+++ src/main/java/com/example/storage_demo/attachment/Attachment.java
@@ -0,0 +1,7 @@
+package com.example.storage_demo.attachment;
+
+import com.example.storage_demo.common.BaseEntity;
+
+public class Attachment extends BaseEntity {
+
+}
---
+++ src/main/java/com/example/storage_demo/common/BaseEntity.java
@@ -0,0 +1,63 @@
+package com.example.storage_demo.common;
+
+import java.util.Date;
+
+import com.example.storage_demo.attachment.storage.KeyDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        Date timestamp = new Date();
+        createdAt = timestamp;
+        updatedAt = timestamp;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public KeyDetails generateKeyDetails(String filename) {
+        KeyDetails details = new KeyDetails();
+        details.setEntityName(this.getClass().getName());
+        details.setEntityId(id);
+        details.setFilename(filename);
+        return details;
+    }
+
+}
